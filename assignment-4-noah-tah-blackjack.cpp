a48ea5b2b2af9066955eb69a87690814de25eb62
Noah Tah <noahtah21@gmail.com>
2025-03-07 19:04:10 -0600

    instructions written

---
+++ assignment-4-noah-tah-blackjack.cpp
@@ -0,0 +1,23 @@
+/*
+Noah Tah
+03/07/2025
+Programming II
+Evan Vaverka
+Northwestern Oklahoma State University
+
+Assignment Description:
+	- Dealer and player both get a hand. Player will go first.
+	- Use a random number generator to give the player two numbers ranging form 2 to 11
+	- Give player option to stay or add another number to try and get closer to 21.
+	- If they go over 21, they bust and lose.
+	- After the player loses or stays, the deaer goes. The dealer has to add new numbers
+	to their original hand until they reach 17 or higher. If they go over 21, they lose.
+	- Once the player and dealer have a number that isn't above 21 and neither have lost,
+	the program will compare the two numbers, with the higher number being the winner.
+	- A tie between numbers results in the dealer winning.
+	- If the player or the dealer manages to get 5 cards in hand and still be less than or
+	equal to 21, its an automatic win for that person. 
+	- That means that the player or dealer cannot have more than 5 cards.
+	- The program should continuously loop by asking the player "Deal another hand?"
+	until they say no.
+*/
